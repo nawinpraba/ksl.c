40f20b44e44d318676887e5eb76fcf9662b8662e
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-07 14:03:58 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the value of a \n");
+scanf("%d",&a);
+printf("enter the power value \n");
+scanf("%d",&b);
+c=pow(a,b);
+scanf("%d",&c);
+getch();
+}
