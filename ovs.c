bf8d85698514aeb7e89c8e331ea37d22a9c39877
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-09 16:10:44 +0530

    Rename n.c to ovs.c

---
+++ ovs.c
@@ -0,0 +1,16 @@
+#include<stdio.h.
+#include,conio.h>
+int main()
+{
+int a,i;
+printf("enter the value\n");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+if(i%2==0)
+{
+printf("%d\n");
+}
+}
+getch();
+}
