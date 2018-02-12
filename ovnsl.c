f5f342032feb9eb048a4279ceb43e73aff73b3cf
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-02-12 13:27:07 +0530

    Rename ovns.c to ovnsl.c

---
+++ ovnsl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+	int n,temp,sum=0,rem=0;
+    scanf("%d",&n);
+    temp=n;
+    while(n!=0)
+{
+    rem=n%10;
+    sum=sum+rem*rem*rem;
+    n=n/10;
+}
+if(sum==temp)
+printf("armstrong number");
+else
+printf("not an armstrong number");
+	return 0;
+}
